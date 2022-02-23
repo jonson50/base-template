179616d218b950fd6a6733f728969008199b2011
John Bermeo <john.bermeo@gmail.com>
2022-02-24 00:22:00 +0100

    comit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'base-template';
+   hideMenu: boolean;
+   wideContent: boolean;
+   innerWidth: any;
+
+   constructor() {
+      this.hideMenu = false;
+      this.wideContent = false;
+   }
+
+   ngOnInit() {
+      this.innerWidth = window.innerWidth;
+      this.setScreen();
+  }
+
+   @HostListener('window:resize', ['$event'])
+   onResize(event: any) {
+      this.innerWidth = window.innerWidth;
+      this.setScreen();
+   }
+
+   setScreen() {
+      if(this.isDesktop()) {
+         this.hideMenu = false;
+         this.wideContent = false;
+      } else {
+         this.hideMenu = true;
+      }
+   }
+
+   isDesktop() {
+      return this.innerWidth < 992 ? false : true;
+   }
+
+   toggleMenu() {
+      this.hideMenu = !this.hideMenu;
+      this.wideContent = !this.wideContent;
+   }
 }
